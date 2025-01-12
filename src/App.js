73683b5b5d4a8d86df37c443f56ca79ee6de16b3
buraktkn <burak.tekin@std.medipol.edu.tr>
2025-01-12 14:02:32 +0300

    Add basic input and button components for weather app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './index.css';
-
+import { useState } from 'react';
 function App() {
+
+  const [city, setCity] = useState(''); // Şehir bilgisi
+  const handleInputChange = (e) => {
+    setCity(e.target.value); //Input alanındaki değeri şehir bilgisi olarak set ediyoruz
+  };
+
+  const handleSearch = () => {
+    console.log("Aranan şehir: ", city); // Şehir bilgisi konsola yazdırılıyor
+  };
+
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <div className="input-container">
+        <input type="text" placeholder="Şehir adını giriniz" value={city} onChange={handleInputChange} />
+        <button onClick={handleSearch}>Arama</button>
+      </div>
+      <div className="weather-result">
+
+      </div>
     </div>
   );
 }
